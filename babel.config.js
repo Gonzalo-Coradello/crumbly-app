2dd270487e080c466fa674319b537f0d832a3c40
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-07-05 15:10:31 -0300

    change babel and typescript config - add import alias for the src folder

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      ['@babel/plugin-transform-flow-strip-types'],
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      [
+        'module-resolver',
+        {
+          alias: {
+            src: './src',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      ],
+    ],
+  }
+}
