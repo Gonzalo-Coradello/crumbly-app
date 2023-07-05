4eb7a3df9b96b433391d5d9b51432210345b3f34
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-07-05 15:18:11 -0300

    separate app styles to its own file

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, View } from 'react-native'
+
+import { Categories, HomeIntro } from './components'
+import { styles } from './styles'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <HomeIntro />
+        <Categories />
+      </View>
+    </SafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
---
+++ src/styles.ts
@@ -0,0 +1,8 @@
+import { StatusBar, StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    marginTop: StatusBar.currentHeight,
+  },
+})
