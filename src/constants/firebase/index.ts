4d012ef84ec66bbad4a228736ace79052df88e13
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-08-17 08:41:01 -0300

    update .gitignore

---
+++ src/constants/firebase/index.ts
@@ -0,0 +1,3 @@
+export const FIREBASE_BASE_URL = 'https://crumbly-app-default-rtdb.firebaseio.com/'
+export const AUTH_BASE_URL = 'https://identitytoolkit.googleapis.com/v1/'
+export const API_KEY = 'AIzaSyB6utiSj7V7YTZtMkOLlyWWDWJ4VQxEcp0'
