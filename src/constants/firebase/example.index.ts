09fd71461578ad42abd3c4dd8fcef83b05a06401
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-08-30 14:20:09 -0300

    add example file for setting up firebase

---
+++ src/constants/firebase/example.index.ts
@@ -0,0 +1,3 @@
+export const FIREBASE_BASE_URL = 'https://your-app-rtdb.firebaseio.com/' // Firebase Realtime Database
+export const AUTH_BASE_URL = 'https://identitytoolkit.googleapis.com/v1/' // Firebase Auth service
+export const API_KEY = '' // Firebase API key
